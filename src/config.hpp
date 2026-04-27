3eb26dd02bcf9978836e108053b52905826fd4db
Szilágyi Dániel <17363065+LEGIONBOSS@users.noreply.github.com>
2026-04-27 18:37:01 +0200

    Update config.hpp

--- src/config.hpp
+++ src/config.hpp
@@ -8,20 +8,16 @@
 #define CONFIG_H_
 
 // LED
-#define LED_PIN (13)                      // MCU built-in LED pin
+#define LED_PIN (15) // MCU built-in LED pin
 
 // Touch sensor
-#define TOUCH_PIN (19)                    // Touch sensor I/O pin
-#define TOUCH_TIME_MIN (1000)             // Touch sensor minimum detection time
+#define TOUCH_PIN (19)        // Touch sensor I/O pin
+#define TOUCH_TIME_MIN (1000) // Touch sensor minimum detection time
 
 // DFPlayer
-#define MP3PLAYER_UART_PIN_RX (17)        // MCU UART RX -> DFPlayer UART TX
-#define MP3PLAYER_UART_PIN_TX (16)		  // MCU UART TX -> DFPlayer UART RX
-#define MP3PLAYER_SCAN_VOICE_FOLDER (1)   // Scan voice mp3 files folder number
-#define MP3PLAYER_SCAN_VOICE_COUNT (3)    // Scan voice mp3 files count
-#define MP3PLAYER_SCAN_SOUND_FOLDER (2)   // Scan sound mp3 files folder number
-#define MP3PLAYER_SCAN_SOUND_COUNT (1)	  // Scan sound mp3 files count
-#define MP3PLAYER_RESULT_VOICE_FOLDER (3) // Result voice mp3 files folder number
-#define MP3PLAYER_RESULT_VOICE_COUNT (20) // Result voice mp3 files count
+#define MP3PLAYER_UART_PIN_RX (4)  // MCU UART RX -> DFPlayer UART TX
+#define MP3PLAYER_UART_PIN_TX (5)  // MCU UART TX -> DFPlayer UART RX
+#define MP3PLAYER_UART_BAUD (9600) // DFPlayer UART baud rate
+#define MP3PLAYER_SOUND_COUNT (10) // Sound count
 
 #endif // CONFIG_H_
